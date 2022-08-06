5fcaaea9f6ffcadf23a7b8f79cf35a98525fd058
drstone-genius04 <82304884+drstone-genius04@users.noreply.github.com>
2022-08-06 12:23:09 +0530

    first message

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -23,7 +23,7 @@ import gameOverSound from '../assets/sounds/game-over-sound.mp3'
 
 let socket
 // const ENDPOINT = 'http://localhost:5000'
-const ENDPOINT = 'http://localhost:5000'
+const ENDPOINT = 'https://uno-game-allan.herokuapp.com/'
 
 const Game = (props) => {
     const data = queryString.parse(props.location.search)
